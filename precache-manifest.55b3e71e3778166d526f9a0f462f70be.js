ba9532a18a5e302e1476a1998297a512c30ca0a1
AungPhoneKhant <akha0030@student.monash.edu>
2020-07-12 18:18:44 +1000

    Updates

---
+++ precache-manifest.55b3e71e3778166d526f9a0f462f70be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe300c7beca73602cfe1a53b3152dec",
+    "url": "/covid-track/index.html"
+  },
+  {
+    "revision": "0c158e13ba594902ca70",
+    "url": "/covid-track/static/css/main.45105205.chunk.css"
+  },
+  {
+    "revision": "2cb9e4d6677646bce200",
+    "url": "/covid-track/static/js/2.1c0d0c1a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-track/static/js/2.1c0d0c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c158e13ba594902ca70",
+    "url": "/covid-track/static/js/main.27c0dc70.chunk.js"
+  },
+  {
+    "revision": "0707915628edb572b6b7",
+    "url": "/covid-track/static/js/runtime-main.54150e74.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-track/static/media/banner.d7265326.png"
+  }
+]);
